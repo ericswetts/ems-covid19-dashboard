1f6d23b9a19f62588facd7ee9da72ae38a3bb90c
ericswetts <eric.swetts@gmail.com>
2021-01-06 06:08:00 +0300

    Added nightly_update.js and linked to Heroku Scheduler to automate nightly data updates.

--- nightly_update.py
+++ nightly_update.py
@@ -1,9 +1,9 @@
 from db_api_methods import get_from_api, set_to_db
 from process_data import json_to_df, create_chart_ready_df
 
-# if __name__ == '__main__':
-# json = get_from_api()
-# processed_data = json_to_df(json)
-# df = create_chart_ready_df(processed_data)
-# set_to_db(df, 'public.chart_ready')
+if __name__ == '__main__':
+    json = get_from_api()
+    processed_data = json_to_df(json)
+    df = create_chart_ready_df(processed_data)
+    set_to_db(df, 'public.chart_ready')
 
